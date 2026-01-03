2cd5968dc03ee8067d0fe5d6518a4526771d7221
CameronFox01 <cameron22401@icloud.com>
2026-01-03 10:51:49 -0700

    main now reads the file and calls for a random word

--- src/src/PasswordGeneration/Main.java
+++ src/src/PasswordGeneration/Main.java
@@ -1,7 +1,19 @@
 package PasswordGeneration;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String filePath = "src/resources/words_alpha.txt";
+        try {
+            WordDictionary.loadWords(filePath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println(WordDictionary.getRandomWord());
+        }
     }
 }
