ed41ab756b4170aa07b8dda0bec0073af5c60a2b
Cameron Fox <cameron22401@icloud.com>
2026-01-05 12:04:35 -0700

    Made the UI be full screen

--- src/src/UI/UI.java
+++ src/src/UI/UI.java
@@ -1,9 +1,12 @@
 package UI;
 
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+
 
 public class UI extends Application {
 
@@ -14,7 +17,16 @@ public class UI extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Label hello = new Label("Hello World");
-        Scene scene = new Scene(hello, 300, 300);
+        Scene scene = new Scene(hello);
+        Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+
+        // Code for full screen
+        primaryStage.setX(bounds.getMinX());
+        primaryStage.setY(bounds.getMinY());
+        primaryStage.setWidth(bounds.getWidth());
+        primaryStage.setHeight(bounds.getHeight());
+
+        // Basic Code to Show the window
         primaryStage.setScene(scene);
         primaryStage.show();
     }
