b9c0aeed4bfe26ac87358ccb85658b8f43f5b09c
Cameron Fox <cameron22401@icloud.com>
2026-01-05 12:16:57 -0700

    Button created in the UI to generate a password. Only prints to console at the moment

--- src/src/UI/UI.java
+++ src/src/UI/UI.java
@@ -1,13 +1,23 @@
 package UI;
 
+import PasswordGeneration.CreateDictionary;
+import PasswordGeneration.PasswordConfig;
+import PasswordGeneration.PasswordGenerator;
+import PasswordGeneration.WordDictionary;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-
+/**
+ * Tie this in without using anything from Main in the Password Generation.
+ * This will allow me to have a terminal version and a UI version.
+ */
 public class UI extends Application {
 
     public static void main(String[] args) {
@@ -16,8 +26,34 @@ public class UI extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Label hello = new Label("Hello World");
-        Scene scene = new Scene(hello);
+        // Initialize Dictionary
+        String filePath = "src/resources/words_alpha.txt";
+        WordDictionary dictionary = CreateDictionary.createWordDictionary(filePath);
+
+        // Initialize Password Configuration
+        PasswordConfig config = new PasswordConfig();
+
+        // Title for Generate a Password Screen
+        Label title = new Label("Generate a Password");
+        title.setAlignment(Pos.TOP_CENTER);
+        title.setStyle("-fx-font-size: 40px;");
+
+        // Button to Generate a Password
+        Button generateButton = new Button("Generate");
+        generateButton.setStyle("-fx-font-size: 40px;");
+        generateButton.setOnAction(e -> {
+
+            PasswordGenerator generator = new PasswordGenerator(dictionary);
+            String password = generator.generate(config);
+            System.out.println(password);
+        });
+
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        root.setSpacing(10);
+        root.getChildren().addAll(title, generateButton);
+
+        Scene scene = new Scene(root);
         Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
 
         // Code for full screen
